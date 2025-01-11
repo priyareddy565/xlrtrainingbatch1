23b91a19df10ec7c19bdea2dc17f39a39731d7e4
priyareddy565 <kusurupriyareddy35@gmail.com>
2025-01-11 11:45:07 +0530

    Add files via upload

---
+++ Core_Java/Demo26.java
@@ -0,0 +1,18 @@
+package psoo3;
+import java.util.Scanner;
+
+public class Demo26 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a char value: ");
+        char ch = sc.next().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+            System.out.println("vowel");
+        } else {
+            System.out.println("Consonents");
+        }
+        sc.close();
+    }
+    
+}
