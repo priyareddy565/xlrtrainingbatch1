1b4c61bd8ce6ae1727b8ed4b91a44e93cd3f613d
priyareddy565 <kusurupriyareddy35@gmail.com>
2025-01-25 10:56:52 +0530

    Add files via upload

---
+++ java_core/demo041.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class demo041 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+
+        if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || 
+            ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a vowel.");
+        } else if (Character.isLetter(ch)) {
+            System.out.println(ch + " is a consonant.");
+        } else {
+            System.out.println(ch + " is not an alphabet.");
+        }
+    }
+}
+
