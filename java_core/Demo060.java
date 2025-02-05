846826784569c1af8afa87adc898ddfcb1f832fd
priyareddy565 <kusurupriyareddy35@gmail.com>
2025-02-05 10:54:33 +0530

    Add files via upload

---
+++ java_core/Demo060.java
@@ -0,0 +1,30 @@
+public class Demo060 {
+    public static void main(String[] args) {
+       int numberOfTerms = 20; 
+       int[] fibonacciTerms = getFibonacciSequence(numberOfTerms);
+       for (int term : fibonacciTerms) {
+           System.out.print(term + " ");
+       }
+   }
+   
+      
+   public static int[] getFibonacciSequence(int n) {
+       if (n <= 0) {
+           throw new IllegalArgumentException("Number of terms must be greater than zero.");
+       }
+   
+       int[] fibonacci = new int[n];
+       if (n >= 1) {
+           fibonacci[0] = 0; // First term
+       }
+       if (n >= 2) {
+           fibonacci[1] = 1; // Second term
+       }
+   
+       for (int i = 2; i < n ; i++) {        //upto n terms
+           fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+   
+       return fibonacci;
+   }
+}
